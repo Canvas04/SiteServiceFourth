cbdaffebb7c71bd43460d977674c5072476cae60
Ruslan04071996 <ruslan040796@mail.ru>
2020-05-20 20:54:50 +0300

    Сделал второй свайпер на планшетной версии

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -17,8 +17,6 @@ linkShow.addEventListener('click', (ev) => {
     linkShow.style.display = 'none';
     linkHidden.style.display = 'block';
 });
-
-
 linkHidden.addEventListener('click', (ev) => {
     ev.preventDefault();
     linkHidden.style.display = 'none';
@@ -27,3 +25,32 @@ linkHidden.addEventListener('click', (ev) => {
         el.style.display = 'none';
     }
 })
+
+// Прописываю логику для второго свайпера
+let hiddenElRepair = document.querySelectorAll('.services__el_click');
+for (const el of hiddenElRepair) {
+    el.style.display = 'none';
+}
+let linkHiddenSecond = document.querySelector('.services__link-block_down');
+linkHiddenSecond.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    linkHiddenSecond.style.display ='none';
+    linkShowSecond.style.display = 'block';
+    for (const el of hiddenElRepair) {
+        el.style.display = 'flex';
+    }
+})
+
+let linkShowSecond = document.querySelector('.services__link_up');
+let linkShowSecondBlock = document.querySelector('.services__link-block_down')
+linkShowSecond.style.display = 'none';
+linkShowSecond.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    for (const el of hiddenElRepair) {
+        el.style.display = 'none';
+        
+        }
+linkShowSecondBlock.style.display = 'none';
+    linkShowSecond.style.display = 'none';
+    linkHiddenSecond.style.display = 'block';
+});
