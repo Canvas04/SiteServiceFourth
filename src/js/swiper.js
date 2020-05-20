021ea880e332624641417f3d1b21594277711db8
Ruslan04071996 <ruslan040796@mail.ru>
2020-05-20 12:24:05 +0300

    Сделал полностью первый свипер на планшетной верси

---
+++ src/js/swiper.js
@@ -0,0 +1,29 @@
+let hiddenEl = document.querySelectorAll('.logo-of-companies__el_click');
+for (const el of hiddenEl) {
+    el.style.display = 'none'
+}
+let linkHidden = document.querySelector('.logo-of-companies__link-block_up');
+linkHidden.style.display = 'none';
+
+let linkShow = document.querySelector('.logo-of-companies__link_down');
+linkShow.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    for (const el of hiddenEl) {
+        el.style.display = 'flex';
+        el.style.justifyContent = 'space-between';
+        el.style.marginTop = '-5px';
+    }
+
+    linkShow.style.display = 'none';
+    linkHidden.style.display = 'block';
+});
+
+
+linkHidden.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    linkHidden.style.display = 'none';
+    linkShow.style.display = 'block';
+    for (const el of hiddenEl) {
+        el.style.display = 'none';
+    }
+})
