7198431adf38bc4123731248cae87983f1920f6e
Ruslan04071996 <ruslan040796@mail.ru>
2020-05-27 21:14:36 +0300

    Сделал анимацию одного модального окна

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,9 +2,7 @@ import { mainContent, footer, popupMenu } from './popup.js';
 
 let modalEl = document.querySelector('.modal');
 modalEl.style.position = 'absolute';
-modalEl.style.left = '-1000px';
-modalEl.style.transitionProperty = 'left';
-modalEl.style.transitionDuration = '2s';
+modalEl.classList.add('modal_position-commerical-md-before')
 
 const closeModalEl = document.querySelector('.modal__icon')
 const commercialLink = document.querySelector('.warn__link');
@@ -18,21 +16,25 @@ const listenerFuncCommercial = function (ev) {
     ev.preventDefault();
     modalHeaderEl.textContent = 'Обратная связь';
 
-for(let el of modalHiddenInput ) {
-    el.style.display = 'block';
-    
-}    
+    for (let el of modalHiddenInput) {
+        el.style.display = 'block';
+
+    }
     mainContent.style.display = 'none';
     footer.style.display = 'none';
-    modalEl.style.left = '0px';
+    modalEl.classList.remove('modal_position-commerical-md-before')
+    modalEl.classList.add('modal_position-commerical-md-after')
+
 }
 commercialLink.addEventListener('click', listenerFuncCommercial);
 
-closeModalEl.addEventListener('click', function (ev) {
-    modalEl.style.left = '-1100px';
 
-    modalEl.style.transitionProperty = 'left';
-    modalEl.style.transitionDuration = '2s';
+
+
+closeModalEl.addEventListener('click', function (ev) {
+    modalEl.classList.remove('modal_position-commerical-md-after');
+    modalEl.classList.remove('modal_position-commerical-md-before')
+    modalEl.classList.add('modal_position-commercial-md-after-animation');
 
     mainContent.style.display = 'block';
     footer.style.display = 'flex';
@@ -60,6 +62,7 @@ if (matchMedia) {
 }
 function screenWidth(mq) {
     if (mq.matches) {
+       modalEl.classList.add('modal_position-commerical-lg-before')
         for (const el of orderCallBtn) {
             el.addEventListener('click', function (ev) {
                 modalEl.style.display = 'block';
@@ -78,21 +81,23 @@ function screenWidth(mq) {
 
         }
         closeModalEl.addEventListener('click', function (ev) {
-            // modalEl.style.display = 'none';
-            modalEl.style.left = '-1000px';
-            modalEl.style.transitionProperty = 'left';
-            modalEl.style.transitionDuration = '2s';
+
+            // modalEl.style.left = '-1000px';
+            // modalEl.style.transitionProperty = 'left';
+            // modalEl.style.transitionDuration = '2s';
 
             mainContent.style.opacity = 1;
             footer.style.opacity = 1;
             popupMenu.style.opacity = 1;
         });
         commercialLink.addEventListener('click', function (ev) {
+            ev.preventDefault();
+            commercialLink.removeEventListener('click', listenerFuncCommercial)
+            modalEl.classList.remove('modal_position-commerical-md-after');
+            modalEl.classList.remove('modal_position-commerical-md-before');
+            modalEl.classList.add('modal_position-commerical-lg-before');
+            modalEl.classList.add('modal_position-commerical-lg-after')
             modalHeaderEl.textContent = 'Обратная связь';
-            modalEl.style.display = 'block';
-            modalEl.style.position = 'absolute';
-            modalEl.style.right = 0;
-            modalEl.style.top = 0;
             mainContent.style.display = 'block';
             mainContent.style.opacity = 0.01;
             footer.style.display = 'block';
